5c03f5cc1521a54462a738e0f812cf910976920a
mrehanamjad <rehanamjad814@gmail.com>
2025-06-23 23:50:03 +0500

    fix errors

--- src/app/api/imagekit/imagekit-auth/route.ts
+++ src/app/api/imagekit/imagekit-auth/route.ts
@@ -1,21 +1,21 @@
 import ImageKit from "imagekit"
 import { NextResponse } from "next/server";
 
-export const imagekit = new ImageKit({
+const imagekit = new ImageKit({
   publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
   urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
 });
 
 export async function GET() {
- try {
-    const authParameters = imagekit.getAuthenticationParameters()
-     return NextResponse.json(authParameters);
- } catch (error) {
-  console.log("Error in ImageKit authentication:", error);
+  try {
+    const authParameters = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authParameters);
+  } catch (error) {
+    console.error("Error in ImageKit authentication:", error);
     return NextResponse.json(
-        {error: "Imagekit Failed" },
-        { status: 500 }
-    )
- }
+      { error: "Imagekit Failed" },
+      { status: 500 }
+    );
+  }
 }
