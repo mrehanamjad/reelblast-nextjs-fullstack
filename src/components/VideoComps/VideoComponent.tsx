87c6eeeeecf0a71e68a55b6f7d2d819e66bcc677
mrehanamjad <rehanamjad814@gmail.com>
2025-07-01 04:52:40 +0500

    fix: adjust video component positioning for improved interaction

--- src/components/VideoComps/VideoComponent.tsx
+++ src/components/VideoComps/VideoComponent.tsx
@@ -576,7 +576,7 @@ export default function VideoComponent({
     <div className="relative h-screen  w-full my-auto mx-auto max-w-sm bg-black overflow-hidden snap-center">
       {/* Full Screen Video */}
       <div
-        className="absolute inset-0 z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        className="absolute inset-0 z-0 "
         onClick={togglePlayPause}
         onDoubleClick={handleDoubleTap}
       >
